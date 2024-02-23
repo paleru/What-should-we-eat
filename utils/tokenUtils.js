a1f7a68e4e3545920e9ac8218bd33fc1fcfb9546
Paal <paru001@student.kristiania.no>
2024-02-23 11:11:53 +0100

    more logging

--- utils/tokenUtils.js
+++ utils/tokenUtils.js
@@ -2,7 +2,8 @@ import jwt from 'jsonwebtoken';
 
 // Create and verify tokens using jsonwebtoken and environment variables
 export const createToken = (payload) => {
-  console.log('environment variables:', process.env);
+  console.log('environment variables inside tokenUtils:', process.env);
+  console.log(process.env.JWT_SECRET, process.env.JWT_EXPIRES_IN);
   const token = jwt.sign(payload, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRES_IN,
   });
